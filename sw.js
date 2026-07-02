39508404de2632e39e66457e8c80923119cff93e
artjmarcos <artejmarcos@gmail.com>
2026-07-02 02:23:10 -0400

    Implement service worker caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,11 +1,43 @@
+var CACHE_NAME = 'mahjong-tour-v2';
+var urlsToCache = [
+    './',
+    'index.html',
+    'juego.js',
+    'datos.js',
+    'ui.js',
+    'manifest.json',
+    'icon-192.png',
+    'icon-512.png'
+];
+
 self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
     self.skipWaiting();
 });
 
 self.addEventListener('activate', function(event) {
-    event.waitUntil(self.clients.claim());
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
